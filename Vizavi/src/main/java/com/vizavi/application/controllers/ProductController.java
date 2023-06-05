9cf09dc5814bff6495320dccb234f97523b4b2d7
AlexVoiv <118457797+AlexVoiv@users.noreply.github.com>
2023-06-05 11:20:08 +0200

    created productController/DTO/Services etc

--- Vizavi/src/main/java/com/vizavi/application/controllers/ProductController.java
+++ Vizavi/src/main/java/com/vizavi/application/controllers/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
