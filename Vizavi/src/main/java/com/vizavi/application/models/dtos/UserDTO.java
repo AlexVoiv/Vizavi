f9a1172b4d7f030b597a4f1ff27ffae5b38cd5bc
AlexVoiv <118457797+AlexVoiv@users.noreply.github.com>
2023-06-12 15:05:26 +0200

    user dto validation

--- Vizavi/src/main/java/com/vizavi/application/models/dtos/UserDTO.java
+++ Vizavi/src/main/java/com/vizavi/application/models/dtos/UserDTO.java
@@ -1,9 +1,22 @@
 package com.vizavi.application.models.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class UserDTO {
     private Long id;
+
+    @NotBlank(message = "first name must not be blank")
     private String firstName;
+
+    @NotBlank(message = "last name must not be blank")
     private String lastName;
+
+    @NotBlank(message = "email must not be blank")
     private String email;
+    @NotBlank(message = "address must not be blank")
     private String address;
 }
